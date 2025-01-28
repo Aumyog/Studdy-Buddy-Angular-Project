b035d3c71899a8fcf34c4bd7e1aff5241a308a46
Aumyog <aumyogesh10s@gmail.com>
2025-01-28 18:09:53 -0500

    Added home Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { StudyGroupsComponent } from './study-groups/study-groups/study-groups.component';
 import { SignupComponent } from './auth/signup/signup.component';
-
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent },            // Default route for the Login page
+    { path: '', component: HomeComponent },            // Default route for the Login page
     { path: 'login', component: LoginComponent },       // Login page (optional: you can keep or remove this)
     { path: 'study-groups', component: StudyGroupsComponent },
     { path: 'signup', component: SignupComponent }, // Study groups page
   ];
 
  
-
+  
+  
